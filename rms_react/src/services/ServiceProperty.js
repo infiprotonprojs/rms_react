a3cc451c08b08c9b20a520fd0177e42e85853455
Divya <divya@infiproton.com>
2021-02-10 11:38:26 +0530

    url changes

--- rms_react/src/services/ServiceProperty.js
+++ rms_react/src/services/ServiceProperty.js
@@ -1,23 +1,25 @@
 import axios from 'axios';
 
 const property_base_url = "http://localhost:8080/api/v1/users/1/properties";
+const url = "http://localhost:8080/api/v1/properties";
 
 class ServiceProperty {
 
-    createProperty(property){
+    createProperty(property) {
         return axios.post(property_base_url, property);
     }
 
-    getPropertyById(propertyId){
-        return axios.get("http://localhost:8080/api/v1/properties"+'/'+propertyId);
+    getPropertyById(propertyId) {
+        return axios.get(url + '/' + propertyId);
     }
 
-    updateProperty(property, propertyId){
-        return axios.put("http://localhost:8080/api/v1/properties"+'/'+propertyId, property);
+    updateProperty(property, propertyId) {
+        return axios.put(url + '/' + propertyId, property);
     }
 
-    deleteProperty(propertyId){
-        return axios.delete("http://localhost:8080/api/v1/properties"+'/'+propertyId);
+    deleteProperty(propertyId) {
+        return axios.delete(url + '/' + propertyId);
     }
 }
+
 export default new ServiceProperty()
